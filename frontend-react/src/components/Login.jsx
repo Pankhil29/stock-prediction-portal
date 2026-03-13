ca7585cd3c9f49c51fecfcbc81652dd126f5992b
Pankhil29 <pankhilpatel734@gmail.com>
2026-03-13 17:58:27 +0530

    Logout Functionality implemented

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../AuthProvider";
 
 const Login = () => {
-  return <div className="text-light">Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState("");
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const userData = {
+      username,
+      password,
+    };
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/v1/token/",
+        userData,
+      );
+      localStorage.setItem("accessToken", response.data.access);
+      localStorage.setItem("refreshToken", response.data.refresh);
+      setIsLoggedIn(true);
+      console.log("Logged in");
+      navigate("/");
+    } catch (error) {
+      setErrors("invalid credentials");
+      console.log();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6 bg-light-dark p-5 rounded">
+            <h3 className="text-light text-center mb-4">Login to out Portal</h3>
+            <form action="" onSubmit={handleLogin}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control "
+                  placeholder="Set password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              {errors && <div className="text-danger">{errors}</div>}
+              {loading ? (
+                <button
+                  type="submit"
+                  className="btn btn-info d-block mx-auto"
+                  disabled
+                >
+                  <FontAwesomeIcon icon={faSpinner} spin /> Logging in ....
+                </button>
+              ) : (
+                <button type="submit" className="btn btn-info d-block mx-auto">
+                  Login
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
